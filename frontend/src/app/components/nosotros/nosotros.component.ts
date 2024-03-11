a77c1eae15f82a9b3ade4b01d9485a5122b6b366
Fer33U <j3sus3gs@gmail.com>
2024-03-11 13:40:46 -0600

    Nosotros
    Pagina sobre nosotros

--- frontend/src/app/components/nosotros/nosotros.component.ts
+++ frontend/src/app/components/nosotros/nosotros.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [],
+  imports: [
+    CarouselModule
+  ],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.css'
 })
